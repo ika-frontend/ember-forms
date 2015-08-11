7c44c7a4f6ce38893d8cf010d55a149083b93dad
wangmeng <wmeng17@gmail.com>
2015-08-11 10:49:28 +0800

    增加has-property mixin

---
+++ addon/mixins/has-property.js
@@ -0,0 +1,21 @@
+import { Em, Mixin, Binding, assert } from 'ember';
+
+
+export default Mixin.create {
+  property: null,
+
+  propertyName: Em.computed('parentView.property', function() {
+    if(this.get('property')) {
+      return this.get('property');
+    } else if(this.get('parentView.property')) {
+      return this.get('parentView.property');
+    } else {
+      assert(false, 'Property could not be found.');
+    }
+  }),
+
+  init: function() {
+    this.super();
+    Binding.from('model.errors.' + this.get('propertyName').to('errors')).connect(this);
+  }
+}
