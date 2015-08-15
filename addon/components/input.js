05bf971786263fe7639f3fac09029de9d03465be
kkdashu <wmeng17@gmail.com>
2015-08-15 15:07:06 +0800

    把viewId属性从具体的component移到WrapMixin

--- addon/components/input.js
+++ addon/components/input.js
@@ -3,15 +3,5 @@ import FormWrapComponent from './wrap';
 import ControlMixin from '../mixins/control';
 
 export default FormWrapComponent.extend({
-  controlView: Em.TextField.extend(ControlMixin),
-  viewId: Em.computed('control', function() {
-    var controlView = this.get('control');
-    return controlView && controlView.elementId;
-  }),
-  property: void 0,
-  label: void 0,
-  placeholder: void 0,
-  required: void 0,
-  autofocus: void 0,
-  disabled: void 0
+  controlView: Em.TextField.extend(ControlMixin)
 });
--- addon/mixins/wrap.js
+++ addon/mixins/wrap.js
@@ -1,9 +1,25 @@
 import Em from 'ember';
 
 export default Em.Mixin.create({
+  property: void 0,
+  label: void 0,
+  placeholder: void 0,
+  required: void 0,
+  autofocus: void 0,
+  disabled: void 0,
+  viewId: Em.computed('control', function() {
+    var controlView = this.get('control');
+    return controlView && controlView.elementId;
+  }),
+
   init: function() {
     this._super();
   },
+
+  /**
+   * 设置control属性
+   * 指向具体的输入控件
+   **/
   didInsertElement: function() {
      var childViews = this.get('childViews');
      var controlView = childViews.filter(function(view) {
