f505996d6a918c3e832aedc73ceedc946fcdaa4a
kkdashu <wmeng17@gmail.com>
2015-08-15 14:59:14 +0800

    把control公共部分移到controlMixin里面

--- addon/components/input.js
+++ addon/components/input.js
@@ -3,17 +3,7 @@ import FormWrapComponent from './wrap';
 import ControlMixin from '../mixins/control';
 
 export default FormWrapComponent.extend({
-  controlView: Em.TextField.extend(ControlMixin, {
-    attributeBindings: ['placeholder', 'required', 'autofocus', 'disabled', 'name'],
-    placeholder: Em.computed.alias('parentView.placeholder'),
-    required: Em.computed.alias('parentView.required'),
-    autofocus: Em.computed.alias('parentView.autofocus'),
-    disabled: Em.computed.alias('parentView.disabled'),
-    type: Em.computed.alias('parentView.type'),
-    model: Em.computed.alias('parentView.model'),
-    propertyName: Em.computed.alias('parentView.propertyName'),
-    'name': Em.computed.alias('propertyName')
-  }),
+  controlView: Em.TextField.extend(ControlMixin),
   viewId: Em.computed('control', function() {
     var controlView = this.get('control');
     return controlView && controlView.elementId;
--- addon/mixins/control.js
+++ addon/mixins/control.js
@@ -5,6 +5,15 @@ export default Em.Mixin.create({
   hasValue: Em.computed('value', function() {
     return this.get('value');
   }),
+  attributeBindings: ['placeholder', 'required', 'autofocus', 'disabled', 'name'],
+  placeholder: Em.computed.alias('parentView.placeholder'),
+  required: Em.computed.alias('parentView.required'),
+  autofocus: Em.computed.alias('parentView.autofocus'),
+  disabled: Em.computed.alias('parentView.disabled'),
+  type: Em.computed.alias('parentView.type'),
+  model: Em.computed.alias('parentView.model'),
+  propertyName: Em.computed.alias('parentView.propertyName'),
+  'name': Em.computed.alias('propertyName'),
 
   isControl: true,
 
